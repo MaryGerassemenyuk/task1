d3d6dcf70b0a4fbda2fc33ba18398a7ffe9e1c83
MaryGerassemenyuk <masha.1735@gmail.com>
2021-11-29 18:11:34 +0300

    Добавьте файлы проекта.

---
+++ task1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ведите n:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ведите m:");
+            int m = Convert.ToInt32(Console.ReadLine());
+
+            int[] mass = new int[n];
+            int a = 0;
+            for (int i = 0; i < mass.Length; i++)
+            {
+                mass[i] = a + 1;
+                a = mass[i];
+
+            }
+
+            int[] b = new int[m];
+            int[] c = new int[mass.Length];
+
+            while (b[m - 1] != 1)
+            {
+
+                Array.Copy(mass, 0, b, 0, m);
+
+                Array.Copy(mass, m - 1, c, 0, mass.Length - m + 1);
+
+                Array.Copy(mass, 0, c, mass.Length - m + 1, m - 1);
+
+                Console.Write(b[0]);
+
+                Array.Copy(c, 0, mass, 0, n);
+
+            }
+        }
+    }
+}
+
